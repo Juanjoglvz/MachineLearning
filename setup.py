3b2c5841cd66b45fdaeb8a42e9755c57880e509f
JUAN JOSE CORROTO MARTIN <JuanJose.Corroto@alu.uclm.es>
2018-10-04 18:20:56 +0200

    Added cookiecutter directory structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Repository for the project of Machine learning techniques',
+    author='JuanJoseCorroto',
+    license='',
+)
